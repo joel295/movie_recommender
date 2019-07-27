97f265d0443e27bf59933f28a279fcbdf8d9f482
Joel Lawrence <z3331029@unsw.edu.au>
2019-07-27 11:24:57 +1000

    init updated

--- recommender.py
+++ recommender.py
@@ -1,7 +1,13 @@
+# Movie Recommender System
+# Topic ####
+# COMP9417 - Project
+# Written by Joel Lawrence (3331029) and Deepansh Singh ()
+# Last modified, July '19
+
 import pandas as pd
-#import matplotlib.pyplot as plt
 import numpy as np
 import sklearn as sk
+import math
 
 class recommender:
     def __init__(self):
@@ -11,16 +17,23 @@ class recommender:
     
     def populate_user_ratings(self, filePath):
         self.ratingsDF = pd.read_csv(filePath, skiprows=1, sep=',', names=self.ratings_names)
-        
         self.num_users = max(self.ratingsDF.User_ID)
         self.num_movies = max(self.ratingsDF.Movie_ID)
-        print(str(self.num_users) + ' Users in total.' + '\n') 
-        print(str(self.num_movies) + ' Movies in total.' + '\n')
+        print("Users in total: %d\nMovies in total: %d\n" % (self.num_users, self.num_movies))
         
-        
+    def initialise(self):
+        ratings_matrix = np.zeros((self.num_users, self.num_movies))
+        self.entries_counter = 0
+        for rating in self.ratingsDF.itertuples():
+            ratings_matrix[rating[1]-1, rating[2]-1] = rating[3]
+            self.entries_counter += 1
+    
+    ## curation to training and test set here
+
         
 
 
 if __name__ == '__main__':
     recommender = recommender()
     recommender.populate_user_ratings("data/ratings.csv")
+    recommender.initialise()
