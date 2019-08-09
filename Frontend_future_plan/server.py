d2cc2fdb8c5110d968b2a6a96051c6ee1e2926aa
Joel Lawrence <z3331029@unsw.edu.au>
2019-08-09 18:35:36 +1000

    updates

--- Frontend_future_plan/server.py
+++ Frontend_future_plan/server.py
@@ -3,6 +3,7 @@ import io
 from recommender import recommender as rec
 from collections import defaultdict
 
+
 recommender = rec()
 recommender.populate_user_ratings("data/ratings.csv")
 recommender.populate_movie_names("data/movies.csv")
@@ -11,6 +12,10 @@ recommender.populate_movie_url_images("data/imgURLs.csv")
 movie_dict = defaultdict(str)
 url_dict = defaultdict(str)
 user_set = set()
+recommender.initialise()
+recommender.data_processing(0.1)
+recommender.calc_similarity()
+recommender.prediction_using_all_users()
 
 for row in recommender.moviesDF.itertuples():
     movie_dict[int(row[1])] = row[2]
@@ -43,9 +48,28 @@ def home_page():
             link = "static/img/none.png"
         if link == "missing":
             link = "static/img/none.png" 
-        print(link)
         return render_template("result.html", movie=name, url=link, user=who)
     return render_template("home.html", users=users, movies=movies)
+
+@app.route('/top9', methods=['GET', 'POST'])
+def top_9():
+    if request.method == 'POST':
+        result = request.form
+        who = int(result['user'])
+        movies_id = recommender.rating_recommender(who, 9)
+        print(mov)
+        movie_urls = []
+        for id in movies_id:
+            try:
+                movies.append(url_dict[id])
+            except Exception:
+                movies.append("static/img/none.png")
+        movies1 = movie_urls[:3]
+        movies2 = movie_urls[3:6]
+        movies3 = movie_urls[6:]
+        print(movies1)
+        return render_template("top9.html", url1=movies1, url2=movies2,url3=movies3, user_id=who)
+    return render_template("top9_choose.html", users=users)   
 
 @app.route('/about')
 def about_page():
